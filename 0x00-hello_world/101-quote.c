a6d5a7930abccf7957a8e634a4f20a4f25ab2556
uchie333 <uchiematthew@gmail.com>
2023-03-16 15:08:25 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
- * main - A C program that prints a line to the standard error
+ * main -  Entry point
  * Return: 1 (Success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful - Dora Korpar, 2015-10-19\n", stdout);
+        write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
