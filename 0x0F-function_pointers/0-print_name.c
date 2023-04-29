ef52df9903d508e3dac7b21932a61eeed7ca5b73
uchie333 <uchiematthew@gmail.com>
2023-04-29 05:36:02 -0700

    My task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints the name
+ * @name: name of person
+ * @f: function pointer
+ * Return: 0
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+	{
+	return;
+	}
+	else
+	{
+		f(name);
+	}
+}
